ebbb35fdcadcd88671ebdae31b30af72b2a41159
Kevin Zeledón <magneticok@gmail.com>
2021-04-29 23:26:00 -0600

    Pseudoinversa buena :)

--- Catalogo_Grupo_1/pseudoinversa.cpp
+++ Catalogo_Grupo_1/pseudoinversa.cpp
@@ -4,6 +4,13 @@
 
 using namespace arma;
 
+// Este metodo iterativo calcula la pseudoinversa de una matriz y la utiliza para dar
+// solucion al sistema de ecuaciones Ax=B
+// Entradas: A: Matriz de coeficientes
+//           B: Matriz de términos independientes
+//           iterMax: Cantidad maxima de iteraciones
+//           tol: Tolerancia de las iteraciones
+// Salidas: La matriz solucion x al sistema de ecuaciones Ax=B
 Mat<double> pseudoinversa(Mat<double> A, Mat<double> B, int iterMax, double tol){
   int n = A.n_rows;
   int m = A.n_cols;
@@ -13,9 +20,7 @@ Mat<double> pseudoinversa(Mat<double> A, Mat<double> B, int iterMax, double tol)
   
   Mat<double> X, Xo, Xkm1;
   X = (1/alpha)*trans(A);
-  Mat <double> I(n,m, fill::eye);
-
-  cout << I << endl;
+  Mat <double> I(n,m, fill::eye); // MATRIX IDENTIDAD
 
   int k=0;
 
@@ -30,12 +35,12 @@ Mat<double> pseudoinversa(Mat<double> A, Mat<double> B, int iterMax, double tol)
     k=k+1;
   }
 
-  return X;
+  return X*B;
 }
 
 int main(void){
   Mat<double> A = {{1,2,4},{2,-1,1},{1,0,1}};
-  Mat<double> B = {4,3,9};
+  Col<double> B = {4,3,9};
   cout << pseudoinversa(A,B,200, 0.0000000001)<<endl;;
 
   return 0;
