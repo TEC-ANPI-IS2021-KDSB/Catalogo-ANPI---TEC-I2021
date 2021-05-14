f0a4ab0dcadb97146997820147929cd7c9d3a4dc
Kevin Zeledón <magneticok@gmail.com>
2021-05-14 11:54:58 -0600

    Avances en cota_poly_inter

--- Catalogo_Grupo_1/Parte 4/cota_poly_inter.m
+++ Catalogo_Grupo_1/Parte 4/cota_poly_inter.m
@@ -1,33 +1,54 @@
-function cota = cota_poly_inter(funcion, soporte)
+% Funcion que calcula la cota de error del polinomio de interpolacion a partir de la funcion
+% a la cual se le calcula el polinomio y un conjunto de soporte.
+% Entradas: funcion: Funcion a la cual se le va a calcular la cota de error (con x como variable)
+%           soporte: Conjunto de soporte sobre el cual se va a calcular la cota de error
+% Salidas: Cota de error máxima del polinomio de interpolación.
+function cota_poly_inter
+  cota = cota_poly_inter_aux("sin(pi*x/2)",[-1 0 1 2])
+end
+
+function[cota]=cota_poly_inter_aux(funcion, soporte)
   % Simbolico
   pkg load symbolic;
   syms x;
-  fsym = sym(f);
-  f = matlabFunction(sym(f));
+  fsym = sym(funcion);
+  f = matlabFunction(sym(funcion));
 
   % Variables necesarias
   n = length(soporte);
-  derivada1 = matlabFunction(diff(funcion));
-  derivadan = matlabFunction(diff(funcion, n));
+  derivada1 = matlabFunction(diff(fsym));
+  derivadan = matlabFunction(diff(fsym, n));
   
   % Calculando la x maxima
-  posibleYMax = [f(derivada1(soporte(1))) f(derivada1(soporte(2)))];
-  solDiff = transpose(Solve(diff(fsym)==0,x));
+  posibleYMax = [f(soporte(1)) f(soporte(n))];
+  solDiff = transpose(solve(diff(fsym)==0,x))
   for i=1:length(solDiff)
-    posibleYMax = [posibleYMax f(solDiff(i))];
+    posibleYMax = [posibleYMax f(solDiff(i))]
   end
-  xMax = solve(fsym==max(posibleYMax),x)(0);
+  posibleXMax = solve(fsym==max(posibleYMax),x)
+  xMax = solve(fsym==max(posibleYMax),x)(1)
 
   % Calculando ax y la multiplicacion
   posibleDMax = [];
   mult = 1;
   for i=1:n
-    posibleDMax = [posibleDMax derivadan(soporte(i))]
-    mult = mult * (xMax-soporte(i));
+    posibleDMax = [posibleDMax derivadan(soporte(i))];
+    mult = mult * (xMax-soporte(i))
   end
-  ax = max(posibleDMax);
+  ax = max(posibleDMax)
 
   % Calculo final
-  cota = (1/((n+1)!))*ax*mult;
+  cota = (1/(factorial(n+1)))*ax*mult;
 
 end
+
+function xMax = calcXMax(soporte)
+  n = length(soporte);
+  symStr = "1"
+  for i=1:n
+    symStr = strcat(symStr, "*(x-",char(soporte(i)),")")
+  end
+
+end
+
+
