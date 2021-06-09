a333a32c8df5a52ae1e2618572069a7ce1f6d496
Kevin Zeledón <magneticok@gmail.com>
2021-06-08 18:01:39 -0600

    Metodo predictor_corrector y primera version de adam_bashford

--- Catalogo_Grupo_1/Parte 6/predictor_corrector.py
+++ Catalogo_Grupo_1/Parte 6/predictor_corrector.py
@@ -44,7 +44,60 @@ def predictor_corrector(func,a,b,h,yo):
   plt.plot(xk,yk)
   plt.show()
 
+  a_s = divided_diff(xk,yk)[0,:]
+  print(a_s)
+  x = S.symbols('x,y')
+  poly = print_poly(a_s,xk,N)
+
   # Falta el polinomio de interpolacion
-  return [xk,yk]
+  return [xk,yk,poly]
+
+# Metodo que calcula los coeficientes del polinomio de interpolacion
+# a partir del método de diferencias divididas de newton
+# Entradas: x: Vector de valores de x por los que debe pasar el polinomio
+#           y: Vector de valores de y por los que debe pasar el polinomio 
+# Salidas:  Vector de coeficientes a_s del polinomio de interpolacion
+def divided_diff(x, y):
+    '''
+    function to calculate the divided
+    differences table
+
+    Source: https://pythonnumericalmethods.berkeley.edu/notebooks/chapter17.05-Newtons-Polynomial-Interpolation.html
+
+    '''
+    n = len(y)
+    coef = np.zeros([n, n])
+    # the first column is y
+    coef[:,0] = y
+    
+    for j in range(1,n):
+        for i in range(n-j):
+            coef[i][j] = \
+            (coef[i+1][j-1] - coef[i][j-1]) / (x[i+j]-x[i])
+            
+    return coef
+
+# Metodo que se encarga de imprimir el polinomio de interpolacion
+# a partir de la lista de coeficientes del mismo
+# Entradas: Coef: Coeficientes a_s del polinomio de interpolacion
+#           x: Vector de valores de x
+#           n: Longitud de x 
+# Salidas: polinomio de interpolación 
+def print_poly(Coef, x, n):
+    p = []
+
+    for i in range(n):
+        p.append('{}'.format(Coef[i]))
+    contador = 1
+    for k in range(len(x)):
+        for i in range(k+contador, n):
+            p[i] = p[i] + '*(x-{})'.format(x[k])
+        
+        # for j in range(k+contador+1, n-1):
+        #     p[j + 1] = p[j + 1] + '*(x-{})'.format(x[k])
+        
+        # contador += 1
+    pol = "p(x) = " + "+".join(p)
+    return pol
 
 print(predictor_corrector('y-x^2+1',0,2,(2/(11-1)),0.5))
